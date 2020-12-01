f03472710f320c5cb0dca5c7cc290ea4fad47345
gromk <jerome.cardolaccia@gmail.com>
2020-12-01 15:50:17 +0100

    add morning/afternoon feature (compatible with history & total count) + cleaning global variables in main.js

--- html/js/main.js
+++ html/js/main.js
@@ -4,10 +4,29 @@ $(document).ready(function() {
     // DEFINTION OF EVENT HANDLERS
     // ===========================
 
-    // Click event on a schedule cell
-    cell_onClick_handler = function(event) {
-        cell = event.data.cell;
-        changeCellCode(cell, 1, true);
+    // ContextMenu event on a schedule cell
+    cell_onContextMenu_handler = function(event) {
+        event.preventDefault();
+        return false;
+    }
+
+    // MouseDown event on a schedule cell
+    cell_onMouseDown_handler = function(event) {
+        var cell = event.data.cell;
+
+        // Left button => change the cell value
+        if (event.which == 1) {
+            if (cell.hasClass('split'))
+                var half = ((event.clientX - cell.offset().left)/cell.outerWidth() < 0.5 ? "-morning" : "-afternoon");
+            else
+                var half = "";
+            incrementCellCode(cell, 1, half, true);
+        }
+
+        // Right button => split the cell (morning/afternoon) or merge it back (same value for the whole day)
+        else if (event.which == 3) {
+            changeCellSplitting(cell, true);
+        }
     }
 
     // Click event on the name of a group
@@ -28,19 +47,23 @@ $(document).ready(function() {
     ctrlY_handler = function(event) {
         if ($history_pos == 0) return // nothing to redo
 
-        next = $history[$history_pos-1];
+        var next = $history[$history_pos-1];
 
-        if (next.action_id == "change-cell-code") {
-            var old_code = next.data.old_code;
-            var new_code = next.data.new_code;
-            var increment = new_code - old_code;
+        if (next.action_id == "change-cell-value") {
+            var new_code_int = next.data.new_code_int;
             var cell = next.data.cell;
-            changeCellCode(cell, increment, false);
+            var half = next.data.half;
+            changeCellCode(cell, new_code_int, half, false);
         }
 
         else if (next.action_id == "select-user-rows") {
             var new_li = next.data.new_li;
             new_li.triggerHandler("click");
+        }
+
+        else if (next.action_id == "change-cell-splitting") {
+            var cell = next.data.cell;
+            changeCellSplitting(cell, false);
         }
 
         $history_pos -= 1;
@@ -51,19 +74,41 @@ $(document).ready(function() {
     ctrlZ_handler = function(event) {
         if ($history_pos > $history.length - 1) return // nothing to undo
 
-        last = $history[$history_pos];
+        var last = $history[$history_pos];
 
-        if (last.action_id == "change-cell-code") {
-            var old_code = last.data.old_code;
-            var new_code = last.data.new_code;
-            var increment = old_code - new_code;
+        if (last.action_id == "change-cell-value") {
+            var old_code_int = last.data.old_code_int;
             var cell = last.data.cell;
-            changeCellCode(cell, increment, false);
+            var half = last.data.half;
+            changeCellCode(cell, old_code_int, half, false);
         }
 
         else if (last.action_id == "select-user-rows") {
             var old_li = last.data.old_li;
             old_li.triggerHandler("click");
+        }
+
+        else if (last.action_id == "change-cell-splitting") {
+            var cell = last.data.cell;
+
+            // first call to changeCellSplitting (and then changeCellCode) adds the 'pending' class to the cell,
+            // which normally aborts the next call to changeCellCode (because all these send POST requests to the PHP backend)
+            // we use JQuery implementation of Promises to run them sequentially as fast as possible
+            //    several .done() callbacks can be added to the same Deferred object (e.g. result of $.post)
+            //    .done() callbacks are run in the order they were added
+            changeCellSplitting(cell, false)
+            .done(function() {
+
+                // below is a tricky case: undo the merge of two different codes (saved in 'old_state') into a single "unknown" code
+                if (last.data.hasOwnProperty('old_state')) {
+                    var old_code_int_morning = last.data.old_state[0];
+                    var old_code_int_afternoon = last.data.old_state[1];
+                    changeCellCode(cell, old_code_int_morning, "-morning", false)
+                    .done(function() {
+                        changeCellCode(cell, old_code_int_afternoon, "-afternoon", false);
+                    });
+                }
+            });
         }
 
         $history_pos += 1;
@@ -116,41 +161,177 @@ $(document).ready(function() {
     }
 
 
-    // CHANGE THE CODE NUMBER OF A GIVEN CELL
-    // SAVE IT INTO THE DATABASE
-    // UPDATE THE SCHEDULE DISPLAY
-    changeCellCode = function(cell, increment, history=false) {
+    // CHANGE THE CODE NUMBER OF A GIVEN CELL IN THE DATABASE
+    // UPDATE THE SCHEDULE DISPLAY ON SUCCESS
+    changeCellCode = function(cell, new_code_int, half, history=false) {
 
-        // discard the click event if the previous one is still being processed
+        // discard the MouseDown event if the previous one is still being processed
         if (cell.hasClass('pending')) {
             return;
         }
         cell.addClass('pending');
 
-        // retrive the current code number from the CLASS attribute and compute the new one
-        var code_class = cell.attr('class').match(/code-([a-z]+)/)[1];
-        var old_code = OPTIONS.code_classes.indexOf(code_class) ;
-        var new_code = (old_code + increment) % 4 ;
+        // retrieve the code number of the other half (if there is one)
+        // and compute the compound code number to save into the database
+        var new_code_int_db = new_code_int
+        if (cell.hasClass('split')) {
+            var half_is_morning = (half == "-morning");
+            var other_half = half_is_morning ? "-afternoon" : "-morning";
+            var other_code_int = parseCellCode(cell, other_half);
+
+            // the cell won't appear to be split in the database if both codes are the same
+            if (new_code_int_db != other_code_int) {
+                if (half_is_morning)
+                    var new_code_int_db = (10*new_code_int) + other_code_int;
+                else
+                    var new_code_int_db = (10*other_code_int) + new_code_int;
+            }
+        }
 
         // retrieve the date and user from the ID attribute
-        var cell_id = cell.attr('id').substr(5);
-        var cell_xy = cell.attr('id').match(/cell_([0-9]+-[0-9]+-[0-9]+)_([0-9]+)/);
-        var date_str = cell_xy[1];
-        var user_id = cell_xy[2];
+        var cell_id_arr = parseCellId(cell);
+        var cell_id = cell_id_arr[0];
+        var date_str = cell_id_arr[1];
+        var user_id = cell_id_arr[2];
 
         // calls the backend API to change the code number
-        $.post('app.php', {'code': new_code, 'date':date_str, 'user':user_id})
+        var deferred = $.post('app.php', {'code': new_code_int_db, 'date':date_str, 'user':user_id})
              .fail(function(jqXHR) {
                       flash_ajax_error(jqXHR.responseJSON);
                   })
              .done(function(res) {
-                      if (history) addToHistory("change-cell-code", {old_code: old_code, new_code: new_code, cell: cell});  // add the action to history
-                      updateCellDisplay(cell_id, new_code);     // updates the cell display (color, icon...)
-                      updateTotalsForThisDay(date_str, 1);      // updates the totals in header and footer rows
+                      // add the action to history
+                      if (history) {
+                          var old_code_int = parseCellCode(cell, half);
+                          addToHistory("change-cell-value", {old_code_int: old_code_int, new_code_int: new_code_int, half: half, cell: cell});
+                      }
+                      updateCellStyle(cell_id, new_code_int, half);  // updates the cell display (color, icon...)
+                      updateTotalsForThisDay(date_str, 1);           // updates the totals in header and footer rows
                   })
              .always(function() {
                       cell.removeClass('pending');
                     });
+
+        // this allows to chain calls after changeCellCode (because $.post makes it asynchronous !)
+        return deferred;
+    }
+
+    // INCREMENT THE CODE NUMBER OF A GIVEN CELL IN THE DATABASE RELATIVELY TO ITS CURRENT STATE
+    // UPDATE THE SCHEDULE DISPLAY ON SUCCESS
+    incrementCellCode = function(cell, increment, half, history=false) {
+          var old_code_int = parseCellCode(cell, half);
+          var new_code_int = (old_code_int + increment) % OPTIONS.code_classes.length ;
+          changeCellCode(cell, new_code_int, half, history);
+    }
+
+    // SPLIT A CELL SO THAT IT HAS TWO DIFFERENT CODES FOR MORNING/AFTERNOON
+    // OR MERGE IT BACK
+    changeCellSplitting = function(cell, history=false) {
+
+        // discard the MouseDown event if the previous one is still being processed
+        if (cell.hasClass('pending')) {
+            return;
+        }
+
+        // add or remove the 'split' class on the <td> element
+        cell.toggleClass('split');
+
+        // when splitting a cell, we initialize morning and afternoon with the current single-cell code
+        // NOTHING NEEDS TO BE DONE IN THE DATABASE FOR THE TIME BEING, THIS IS JUST DOM MANIPULATION
+        if (cell.hasClass('split')) {
+            // switch from one class to two classes
+            var old_code_classname = cell.attr('class').match(/code-([a-z]+)/)[0];
+            cell.addClass(old_code_classname+"-morning");
+            cell.addClass(old_code_classname+"-afternoon");
+            cell.removeClass(old_code_classname);
+
+            // add a second identical icon in the cell (only the first time it is split)
+            // it will be hidden by CSS if not needed anymore
+            var i = $('i', cell);
+            if (i.length == 1) {
+                var i2 = i.clone();
+                i2.attr('id', 'icon2'+i.attr('id').substr(4));
+                i.addClass('first-icon');
+                i2.addClass('second-icon');
+                cell.append(i2);
+            }
+            // set the second icon exactly as the first one
+            $('.second-icon', cell).attr('class', function(index, cls) {
+                      var fa_class = $('.first-icon', cell).attr('class').match(/fa-[-a-z]+/)[0];
+                      return cls.replace(/fa-[-a-z]+/, fa_class);
+                  })
+
+            if (history) addToHistory("change-cell-splitting", {cell: cell});  // add the action to history
+            var deferred = null;
+            cell.removeClass('pending');
+        }
+
+        // when merging a split cell, set it to "unknown" and update the database
+        // ...unless morning and afternoon have the same code => NOTHING TO DO IN THE DATABASE, JUST DOM MANIPULATION
+        else {
+            var old_code_int_morning = parseCellCode(cell, "-morning");
+            var old_code_int_afternoon = parseCellCode(cell, "-afternoon");
+            var old_code_str_morning = OPTIONS.code_classes[old_code_int_morning];
+            var old_code_str_afternoon = OPTIONS.code_classes[old_code_int_afternoon];
+            var old_code_classname_morning = "code-"+old_code_str_morning+"-morning";
+            var old_code_classname_afternoon = "code-"+old_code_str_afternoon+"-afternoon";
+
+            if (old_code_int_morning == old_code_int_afternoon) {
+                var new_code_classname = "code-"+old_code_str_morning;
+                cell.addClass(new_code_classname);
+                cell.removeClass(old_code_classname_morning);
+                cell.removeClass(old_code_classname_afternoon);
+
+                if (history) addToHistory("change-cell-splitting", {cell: cell});  // add the action to history
+                var deferred = null;
+                cell.removeClass('pending');
+            }
+
+            else {
+                // ambiguous old state is forced to "unknown"
+                var new_code_int = 0;
+
+                // retrieve the date and user from the ID attribute
+                var cell_id_arr = parseCellId(cell);
+                var cell_id = cell_id_arr[0];
+                var date_str = cell_id_arr[1];
+                var user_id = cell_id_arr[2];
+
+                // the backend API needs to be called or this change will be lost
+                cell.addClass('pending');
+                var deferred = $.post('app.php', {'code': new_code_int, 'date':date_str, 'user':user_id})
+                    .fail(function(jqXHR) {
+                              flash_ajax_error(jqXHR.responseJSON);
+                          })
+                    .done(function(res) {
+                              var new_code_classname = "code-"+OPTIONS.code_classes[new_code_int];
+                              var new_code_iconname = "fa-"+OPTIONS.code_icons[new_code_int];
+
+                              cell.addClass(new_code_classname);
+                              cell.removeClass(old_code_classname_morning);
+                              cell.removeClass(old_code_classname_afternoon);
+
+                              $('.first-icon', cell).attr('class', function(index, cls) {
+                                        return cls.replace(/fa-[-a-z]+/, new_code_iconname);
+                                    })
+
+                              // add the action to history
+                              if (history) {
+                                  // an extra parameter 'old_state' is necessary because any old code combination has been replaced by "unknown"
+                                  var old_state = [old_code_int_morning, old_code_int_afternoon];
+                                  addToHistory("change-cell-splitting", {cell: cell, old_state: old_state});
+                              }
+                              //......updateTotalsForThisDay().......
+                          })
+                    .always(function() {
+                              cell.removeClass('pending');
+                            });
+            }
+        }
+
+        // this allows to chain calls after changeCellSplitting (because $.post can make it asynchronous !)
+        return $.when(deferred);
+
     }
 
 
@@ -203,25 +384,53 @@ $(document).ready(function() {
         return e;
     }
 
+    // RETURN THE CURRENT CELL CODE NUMBER BY PARSING ITS CLASS ATTRIBUTE
+    parseCellCode = function(cell, half) {
+        var re = new RegExp('code-([a-z]+)'+half);
+        var code_str = cell.attr('class').match(re)[1];
+        var code_int = OPTIONS.code_classes.indexOf(code_str) ;
+        return code_int;
+    }
+
+    // RETURN THE CELL ID, AND THE CORRESPONDING DATE AND USER IN AN ARRAY
+    parseCellId = function(cell) {
+        var cell_id = cell.attr('id').substr(5);
+        var cell_xy = cell.attr('id').match(/cell_([0-9]+-[0-9]+-[0-9]+)_([0-9]+)/);
+        var date_str = cell_xy[1];
+        var user_id = cell_xy[2];
+        return [cell_id, date_str, user_id];
+    }
+
     // UPDATE THE DISPLAY OF A GIVEN CELL TO MATCH A GIVEN CODE NUMBER
-    updateCellDisplay = function(cell_id, code) {
+    updateCellStyle = function(cell_id, code_int, half) {
+
         // update the <td> class name
         $("#cell_"+cell_id).attr('class', function(i, c) {
-                return c.replace(/code-[a-z]+/, 'code-'+OPTIONS.code_classes[code]);
+                var re = new RegExp('code-([a-z]+)'+half);
+                return c.replace(re, 'code-'+OPTIONS.code_classes[code_int]+half);
             });
 
         // update the <i> class name (font-awesome)
-        $("#icon_"+cell_id).attr('class', function(i, c) {
-                return c.replace(/fa-[-a-z]+/, 'fa-'+OPTIONS.code_icons[code]);
+        if (half == "-afternoon")
+            var icon = $("#icon2_"+cell_id);
+        else
+            var icon = $("#icon_"+cell_id);
+
+        icon.attr('class', function(i, c) {
+                return c.replace(/fa-[-a-z]+/, 'fa-'+OPTIONS.code_icons[code_int]);
             });
     }
 
     // UPDATE THE COUNTS OF CELLS WITH A GIVEN CODE NUMBER
-    updateTotalsForThisDay = function(date, code) {
+    updateTotalsForThisDay = function(date, code_int) {
 
         // jQuery selectors are used to count the cells
+        var code_str = OPTIONS.code_classes[code_int];
         var day_cells = $$("td[id^=cell_"+date+"_]");
-        var count = day_cells.not(".user-hidden").filter(".code-"+OPTIONS.code_classes[code]).length;
+        var count_both = day_cells.not(".user-hidden").filter(".code-"+code_str).length;
+        var count_morning = day_cells.not(".user-hidden").filter(".code-"+code_str+"-morning").length;
+        var count_afternoon = day_cells.not(".user-hidden").filter(".code-"+code_str+"-afternoon").length;
+        var count = Math.max(count_both+count_morning, count_both+count_afternoon);
 
         // update both cells in the header and the footer rows
         var result_cells = $(".cell_total_"+date);
@@ -237,10 +446,10 @@ $(document).ready(function() {
             result_cells.removeClass("warning");
         }
     }
-    updateTotalsForAllDates = function(code) {
+    updateTotalsForAllDates = function(code_int) {
         $$("td[id^=header_]").map(function(index, elt) {
             var date = elt.id.substr(7);
-            updateTotalsForThisDay(date, OPTIONS.code_to_count);
+            updateTotalsForThisDay(date, code_int);
         });
     }
 
@@ -388,7 +597,8 @@ $(document).ready(function() {
                                 td.addClass('code-'+OPTIONS.code_classes[0]);
                                 icon.addClass('fa-'+OPTIONS.code_icons[0]);
 
-                                td.click({'cell': td}, cell_onClick_handler);
+                                td.on('mousedown', {'cell': td}, cell_onMouseDown_handler);
+                                td.on('contextmenu', cell_onContextMenu_handler);
 
                                 td.append(icon);
                             }
@@ -431,8 +641,17 @@ $(document).ready(function() {
                 .done(function(res) {
                     $.each(res.data, function(index, planning) {
                         var cell_id = planning['date']+'_'+planning['user_id'];
-                        var code = planning['code'];
-                        updateCellDisplay(cell_id, code);
+                        var code_int = planning['code'];
+                        if (code_int < 10) {
+                            updateCellStyle(cell_id, code_int, "");
+                        }
+                        else {
+                            var code_morning = Math.floor(code_int/10);
+                            var code_afternoon = code_int % 10
+                            changeCellSplitting($("#cell_"+cell_id), false);
+                            updateCellStyle(cell_id, code_morning, "-morning");
+                            updateCellStyle(cell_id, code_afternoon, "-afternoon");
+                        }
                     });
 
                     // Compute and display the number of people on the working place
